b948613d16d5548c892fac783622fbf207b8139a
Matthew <matthewbegley@nhs.net>
2020-05-12 17:20:27 +0100

    initial_commit_wip

---
+++ infrastructure/modules/rds/dos_db.tf
@@ -0,0 +1,97 @@
+
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = var.db_subnet_group_name
+  subnet_ids = var.private_subnets_ids
+  tags = {
+    Name        = "${var.service_prefix}-${var.cloud_env_type}-db"
+    BillingCode = var.billing_code_tag
+    Environment = var.environment_tag
+    Version     = var.version_tag
+    Service     = var.service_name
+  }
+}
+
+module "db" {
+    source = "terraform-aws-modules/rds/aws"
+    version = "~> 2.0"
+
+    identifier = "${var.service_prefix}-${var.cloud_env_type}-db"
+
+    engine = "postgres"
+    engine_version = "11.6"
+    instance_class = "db.t3.micro"
+    allocated_storage = 10
+
+    name     = var.db_name
+    username = "user"
+    password = "YourPwdShouldBeLongAndSecure!"
+    port     = "5432"
+
+    #iam_database_authentication_enabled = true
+
+    vpc_security_group_ids = ["${var.vpc_security_group}"]
+
+    maintenance_window = "Mon:00:00-Mon:03:00"
+    backup_window      = "03:00-06:00"
+
+    # disable backups to create DB faster
+    backup_retention_period = 0
+
+    tags = {
+        Owner       = "user"
+        Name        = "${var.service_prefix}-${var.cloud_env_type}-db"
+        BillingCode = var.billing_code_tag
+        Environment = var.environment_tag
+        Version     = var.version_tag
+        Service     = var.service_name
+    }
+
+  # DB subnet group (create own)
+  subnet_ids = var.private_subnets_ids
+  
+
+  # DB parameter group
+  family = "postgres11"
+
+  # DB option group
+  major_engine_version = "11.6"
+
+  # Snapshot name upon DB deletion
+  final_snapshot_identifier = "${var.service_name}_dos_db"
+
+  # Database Deletion Protection
+  deletion_protection = false
+
+  parameters = [
+    {
+      name         = "max_connections"
+      value        = var.db_max_connections
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "client_encoding"
+      value        = "UTF8"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "rds.force_ssl"
+      value        = "1"
+      apply_method = "immediate"
+    },
+    {
+      name         = "timezone"
+      value        = "GB"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "rds.log_retention_period"
+      value        = "5760"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "ssl"
+      value        = "1"
+      apply_method = "immediate"
+    }
+  ]
+}
---
+++ infrastructure/stacks/service/main.tf
@@ -0,0 +1,57 @@
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = var.terraform_platform_state_s3_bucket
+    key    = var.vpc_terraform_state_key
+    region = var.aws_region
+  }
+}
+
+data "terraform_remote_state" "security-groups-k8s" {
+  backend = "s3"
+
+  config = {
+    bucket = var.terraform_platform_state_s3_bucket
+    key    = var.security-groups-k8s_terraform_state_key
+    region = var.aws_region
+  }
+}
+
+resource "aws_security_group" "rds-postgres-sg" {
+  name        = "${var.service_prefix}-${var.cloud_env_type}-db-sg"
+  description = "Allow connection by appointed rds postgres clients"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  tags = {
+    Name        = "Security Group for access to RDS Postgres"
+    BillingCode = var.billing_code_tag
+    Environment = var.environment_tag
+    Version     = var.version_tag
+    Programme   = var.nhs_programme_name
+    Project     = var.nhs_project_name
+    Terraform   = "true"
+    Service     = var.service_name
+  }
+}
+
+module "dos_db" {
+  source             = "../../modules/rds"
+  service_prefix     = var.service_prefix
+
+  db_name                 = var.db_name
+  private_subnets_ids     = data.terraform_remote_state.vpc.outputs.private_subnets
+  cloud_env_type          = var.cloud_env_type
+
+
+  billing_code_tag   = var.billing_code_tag
+  environment_tag    = var.environment_tag
+  version_tag        = var.version_tag
+  nhs_programme_name = var.nhs_programme_name
+  nhs_project_name   = var.nhs_project_name
+  service_name       = var.service_name
+
+  vpc_security_group = aws_security_group.rds-postgres-sg.id
+  db_max_connections = var.db_max_connections
+}
