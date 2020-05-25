7361b684c0bf65464572f27cee4a3defc1aa0b62
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2020-05-25 08:03:57 +0100

    Update the DevOps automation toolchain scripts (#6)

--- build/automation/lib/python.mk
+++ build/automation/lib/python.mk
@@ -7,14 +7,18 @@ PYTHON_BASE_PACKAGES = \
 	flake8 \
 	mypy \
 	pygments \
-	pylint
+	pylint \
+	pyyaml
 
 python-virtualenv: ### Setup Python virtual environment - optional: PYTHON_VERSION
+	brew update
+	brew upgrade pyenv
 	pyenv install --skip-existing $(PYTHON_VERSION)
 	pyenv virtualenv --force $(PYTHON_VERSION) $(PROJECT_GROUP_SHORT)-$(PROJECT_NAME)
 	pyenv local $(PROJECT_GROUP_SHORT)-$(PROJECT_NAME)
 	pip install --upgrade pip
 	pip install $(PYTHON_BASE_PACKAGES)
+	ln -sfv ~/.pyenv/versions/$(PYTHON_VERSION) ~/.pyenv/versions/default
 
 python-virtualenv-clean: ### Clean up Python virtual environment - optional: PYTHON_VERSION
 	rm -rf \
