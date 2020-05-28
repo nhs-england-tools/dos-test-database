9f412d6502d07183315fc72ee3ef420e43e3c924
Jack Plowman <jack.plowman1@nhs.net>
2020-05-28 10:47:37 +0100

    Adding storage encryption and formatting

--- infrastructure/modules/rds/dos_db.tf
+++ infrastructure/modules/rds/dos_db.tf
@@ -1,36 +1,37 @@
 module "db" {
-    source = "terraform-aws-modules/rds/aws"
-    version = "~> 2.0"
+  source  = "terraform-aws-modules/rds/aws"
+  version = "~> 2.0"
 
-    identifier = "${var.instance_db_name}-${var.cloud_env_type}"
+  identifier = "${var.instance_db_name}-${var.cloud_env_type}"
 
-    engine = "postgres"
-    engine_version = "11.6"
-    instance_class = "db.t3.micro"
-    allocated_storage = 10
+  engine            = "postgres"
+  engine_version    = "11.6"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 10
+  storage_encrypted = true
 
-    name     = var.db_name
-    username = "postgres"
-    password = aws_secretsmanager_secret_version.dos_test_db_password.secret_string
-    port     = "5432"
+  name     = var.db_name
+  username = "postgres"
+  password = aws_secretsmanager_secret_version.dos_test_db_password.secret_string
+  port     = "5432"
 
-    #iam_database_authentication_enabled = true
+  #iam_database_authentication_enabled = true
 
-    vpc_security_group_ids = ["${var.vpc_security_group}"]
+  vpc_security_group_ids = ["${var.vpc_security_group}"]
 
-    maintenance_window = "Mon:00:00-Mon:03:00"
-    backup_window      = "03:00-06:00"
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
 
-    # disable backups to create DB faster
-    backup_retention_period = 0
+  # disable backups to create DB faster
+  backup_retention_period = 0
 
-    tags = {
-        Name        = "${var.instance_db_name}-${var.cloud_env_type}"
-        BillingCode = var.billing_code_tag
-        Environment = var.environment_tag
-        Version     = var.version_tag
-        Service     = var.service_name
-    }
+  tags = {
+    Name        = "${var.instance_db_name}-${var.cloud_env_type}"
+    BillingCode = var.billing_code_tag
+    Environment = var.environment_tag
+    Version     = var.version_tag
+    Service     = var.service_name
+  }
 
   # DB subnet group (create own)
   subnet_ids = var.private_subnets_ids
