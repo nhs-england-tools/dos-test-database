85860479d666bbda7b4b579d23b0a116503a0715
Matthew <matthewbegley@nhs.net>
2020-05-29 12:32:49 +0100

    K8s deploy scripts fixes

--- build/automation/var/profile/non-prod.mk
+++ build/automation/var/profile/non-prod.mk
@@ -4,7 +4,7 @@
 # Service variables
 
 DB_NAME=postgres
-DB_HOST=test-rds-instance-nonprod.cqger35bxcwy.eu-west-2.rds.amazonaws.com
+DB_HOST=test-dos-db-4-rds-nonprod.cqger35bxcwy.eu-west-2.rds.amazonaws.com
 DB_PORT=5432
 DB_USERNAME=postgres
 #DB_PASSWORD=password
