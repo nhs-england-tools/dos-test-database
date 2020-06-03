35aef1322cf1f773b1b070b206a0bc793fe2782f
Matthew <matthewbegley@nhs.net>
2020-06-03 16:52:51 +0100

    ssl k8s worker to rds fix

--- build/automation/var/profile/non-prod.mk
+++ build/automation/var/profile/non-prod.mk
@@ -3,8 +3,8 @@
 # ==============================================================================
 # Service variables
 
-DB_NAME=postgres
-DB_HOST=test-dos-db-4-rds-nonprod.cqger35bxcwy.eu-west-2.rds.amazonaws.com?sslmode=require
+DB_NAME=postgres?sslmode=require
+DB_HOST=test-dos-db-4-rds-nonprod.cqger35bxcwy.eu-west-2.rds.amazonaws.com
 DB_PORT=5432
 DB_USERNAME=postgres
 #DB_PASSWORD=password
